97c77ec60583cc974b7841ad8919517e90aa3e06
MdAtikBhuiyan <touhidatik81@gmail.com>
2021-01-30 14:39:45 +0600

    assignment-Done

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 
-function getSeatnumber(seatId){
+function getSeatNumber(seatId){
     const seat = document.getElementById(seatId);
     const seatNumber = parseInt(seat.value);
     return seatNumber;
 }
 
-function updateAmount(seatInputId, isIncreasing){
-    let seatNumber = getSeatnumber(seatInputId);
+// seat count
+
+function updateSeatCount(seatInputId, isIncreasing){
+    let seatNumber = getSeatNumber(seatInputId);
     if (isIncreasing == true){
         seatNumber = seatNumber + 1;
     }
@@ -14,22 +16,14 @@ function updateAmount(seatInputId, isIncreasing){
         seatNumber = seatNumber - 1;
     }
     document.getElementById(seatInputId).value = seatNumber;
-
-    let subTotal = 0;
-
-    if (seatInputId == "firstClassInput"){
-        subTotal = seatNumber * 150;
-    }
-    if(seatInputId == "economyClassInput"){
-        subTotal = seatNumber * 100;
-    }
-    document.getElementById("sub-total").innerText = subTotal;
     calculateTotal();
 }
 
+// total bill 
+
 function calculateTotal(){
-    const firstClassSeat = getSeatnumber("firstClassInput");
-    const economyClassSeat = getSeatnumber("economyClassInput");
+    const firstClassSeat = getSeatNumber("firstClassInput");
+    const economyClassSeat = getSeatNumber("economyClassInput");
 
     const subTotal = (firstClassSeat * 150) + (economyClassSeat * 100);
     document.getElementById("sub-total").innerText = subTotal;
@@ -48,10 +42,10 @@ function confirmationMessage(){
     document.getElementById('all-content').style.display = 'none';
     document.getElementById('confirmation-box').style.display = 'block';
 
-    const firstClassSeat = getSeatnumber("firstClassInput");
+    const firstClassSeat = getSeatNumber("firstClassInput");
     document.getElementById("forFirstClassSeatMesage").innerText = firstClassSeat;
 
-    const economyClassSeat = getSeatnumber("economyClassInput");
+    const economyClassSeat = getSeatNumber("economyClassInput");
     document.getElementById("forEconomyClassSeatMesage").innerText = economyClassSeat;
 
     const subTotalForFirstClass = (firstClassSeat * 150) ;
@@ -70,25 +64,4 @@ function confirmationMessage(){
     document.getElementById("forTotalMesage").innerText = total;
 }
 
-
-    
-        // const flightDetails = document.querySelectorAll(".flight-deatails");
-        // const fromFlyingPlace = document.getElementById("fromflyingPlace").value;
-        //         const toFlyingPlace = document.getElementById("toflyingPlace").value;
-        //         const departureDate = document.getElementById("departureDate").value;
-        //         const returnDate = document.getElementById("returnDate").value;
-                
-        // // for(let i=0; i<flightDetails.length; i++){
-        // //     let details = flightDetails [i];
-        // //     if((flightDetails[i] != "" && flightDetails[i+1] != "" && flightDetails[i+2] != "" && flightDetails[i+3] != "" )){
-                
-        // //         calculateTotal();
-        // //         console.log(fromFlyingPlace,toFlyingPlace,departureDate,returnDate);
-        // //     }
-        // //     if(flightDetails[i] == "" || flightDetails[i+1] == "" || flightDetails[i+2] == "" || flightDetails[i+3] == "" ){
-        // //         console.log('input please');
-        // //     }
-            
-        // // }
-        
         
